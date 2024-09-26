0f9f8dbb2bd91a5b27bc2e52b67624e97684b84f
k81h <madhureemadaa09@gmail.com>
2024-09-26 10:47:18 +0530

    Fix service worker path

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ openDB();
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("/service-worker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
